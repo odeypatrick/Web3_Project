aaa5df5a9a8909151a281065a68ad6bcc46eb3f1
Patrick Odey Joseph <odeydev@gmail.com>
2022-05-17 07:25:52 +0100

    intitial commit

--- src/routes/verify/Verify.jsx
+++ src/routes/verify/Verify.jsx
@@ -1,24 +1,38 @@
 import { useState } from "react";
 import BarcodeScannerComponent from "react-qr-barcode-scanner";
+import './Verify.css'
 
 const Verify = () => {  
-    const [data, setData] = useState("Not Found");    
+    const [data, setData] = useState("Not Found");
+    const [torchOn, setTorchOn] = useState(false);   
 
     return (
         <div className="verify-page">
+            <div className="container">
             <h1>Verify Code</h1>
-            <div className="wrapper">
-            <>
-            <BarcodeScannerComponent
-                width={500}
-                height={500}
-                onUpdate={(err, result) => {
-                if (result) setData(result.text);
-                else setData("Not Found");
-                }}
-            />
-            <p>{data}</p>
-            </>
+            <div className="wrapper flex">
+                <div className="scan">
+                <BarcodeScannerComponent
+                        width={350}
+                        height={350}
+                        torch={torchOn}
+                        onUpdate={(err, result) => {
+                            if (result) {
+                                setData(result.text)
+                            }
+                            else {
+                                setData("Not Found")
+                                // setError("Not recognised")
+                            };
+                        }}
+                    />
+                <p>{data}</p>
+                </div>
+
+                <div className="product-info">
+                    <h2>Product details</h2>
+                </div>
+            </div>
             </div>
         </div>
     )
